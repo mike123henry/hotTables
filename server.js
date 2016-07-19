6e335dadcbc039a30cd733e41d522f9d215bab73
MikeH <mike123.henry@gmail.com>
2016-07-19 06:53:02 -0500

    added server on port 3000 code and routes to the html pages working

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ You should be referencing the code from the previous Star Wars application.
 
 Feel encouraged to use the following application as a reference: http://hot-restaurant.herokuapp.com/
 
-Note: We know this is a hard activity. We know you aren't yet comfortable with Node or Express. But push yourself. The best way to learn is to push through the discomfort and BUILD! Ask for help when you need it. We're here to help you through the process. */ 
+Note: We know this is a hard activity. We know you aren't yet comfortable with Node or Express. But push yourself. The best way to learn is to push through the discomfort and BUILD! Ask for help when you need it. We're here to help you through the process. */
 
 var express= require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var PORT = 3000;
+app.set("port", process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -64,35 +64,40 @@ var tables = [
 	email: "",
 	uniqueId: ""
 }
-]
-var counter = 0; 
+];
+var counter = 0;
 
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'view.html'));
 })
 
-app.get('/add', function(req, res){
-	res.sendFile(path.join(__dirname, 'add.html'));
+app.get('/tables', function(req, res){
+	res.sendFile(path.join(__dirname, 'tables.html'));
 })
 
-app.get('/all', function(req, res){
-	res.sendFile(path.join(__dirname, 'all.html'));
+app.get('/reserve', function(req, res){
+	res.sendFile(path.join(__dirname, 'reserve.html'));
 })
 
-app.get('/api/:tables?', function(req, res){
-	var currentTable = req.params.tables;
-	//console.log(req);
-	if(currentTable){
-		console.log(currentTable);
-		for(var i = 0; i < tables.length; i++){
-			if()
-		}
-	}
-} )
+// app.get('/api/:tables?', function(req, res){
+// 	var currentTable = req.params.tables;
+// 	//console.log(req);
+// 	if(currentTable){
+// 		console.log(currentTable);
+// 		for(var i = 0; i < tables.length; i++){
+// 			//if()
+// 		}
+// 	}
+// } )
 
 
 
 
+
+
+app.listen(app.get('port'),function(){
+    console.log('Experss started on http://localhost:' + app.get('port') +" press cntrl-C to exit");
+});
 
 
 
